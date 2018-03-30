e5a4b662c3df0af9e8edd8ede0b6d49569a38242
Reilley <reilleyford@gmail.com>
2018-03-29 23:10:31 -0400

    minor changes

--- app.js
+++ app.js
@@ -3,9 +3,11 @@ const mysql = require('mysql');
 const config = require('./config.js');
 const rf = require('./modules/readFile.js');
 
+//database variables that can be changed to make it dynamic
 const table = 'Comics';
 const delimiter = '\t';
 
+//opening a connection to a MySQL db using node-mysql
 const conn = mysql.createConnection({
   host: config.DB_HOST,
   port: config.DB_PORT,
@@ -16,12 +18,18 @@ const conn = mysql.createConnection({
   charset: config.DB_ENC
 });
 
+//readline interface for grabbing user input
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
 
 
+/**
+ * insertIntoDB function takes an object of records parsed from ./modules/readFile
+ * it will format and build the sql queries and then execute all of them.
+ * minor error handling will log each query executed and which ones it skips/errors out
+ **/
 insertIntoDB = (rows) => {
   conn.connect((err) => {
     if(err) throw err;
@@ -62,6 +70,10 @@ insertIntoDB = (rows) => {
   });
 }
 
+/**
+ * kicks off the app, grabs the filepath, passes that and insertIntoDB function
+ * as a callback to ./modules/readFile.js.
+ **/
 rl.question('Enter file path: ', (answer) => {
   answer = answer.trim()
   rf.readFile(answer, insertIntoDB)
--- modules/readFile.js
+++ modules/readFile.js
@@ -1,6 +1,9 @@
 const fs = require('fs');
 
-//readFile method that accepts a filepath and a callback
+/**
+ * readFile method that accepts a filepath and a callback, reads the file
+ * and passes it to a callback
+ **/
 module.exports.readFile = (file, cb) => {
   fs.readFile(file, 'utf8', (err, data) => {
     if(err) throw err;
